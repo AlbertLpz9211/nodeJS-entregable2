79fae3929a65ec7cccd3012e4c80e562a2cf35e3
AlbertLpz9211 <alberto.lopez_g@outlook.com>
2023-01-09 17:12:37 -0600

    finish the relations models

--- src/models/init.model.js
+++ src/models/init.model.js
@@ -1,17 +1,20 @@
 //se tienen que importar todos nuestros modelos creados
 const Todos = require("./todos.model");
-const Todos_categories = require("./todos-categories.model");
+const TodosCategories = require("./todos-categories.model");
 const Users = require("./users.model");
 const Categories = require("../models/categories.model");
 const initModels = () => {
-  Todos_categories;
-  Categories;
   //vamos a crear las relaciones
   //hasOne --> tiene a uno
   //hasMany --> tiene muchos
   //belongsTo --> pertenece a
   Todos.belongsTo(Users, { as: "author", foreignKey: "user_id" }); //una tarea pertenece a un solo usuario --> con el belongsTo, sequilize ya sabe que tiene la llave foranea
   Users.hasMany(Todos, { as: "task", foreignKey: "user_id" }); // un usuario tiene muchas tareas
+  //Relacion many to many categories y todos
+  TodosCategories.belongsTo(Todos, { as: "task", foreignKey: "todo_id" });
+  Todos.hasMany(TodosCategories, { as: "category", foreignKey: "todo_id" });
+  TodosCategories.belongsTo(Categories, { as: "category", foreignKey: "category_id" });
+  Categories.hasMany(TodosCategories, {as: "task", foreignKey: "category_id"});
 };
 
 module.exports = initModels;
--- src/models/todos-categories.model.js
+++ src/models/todos-categories.model.js
@@ -1,8 +1,10 @@
 const db = require("../utils/database");
 
 const { DataTypes } = require("sequelize");
+const Categories = require("./categories.model");
+const Todos = require("./todos.model");
 
-const Todos_categories = db.define(
+const TodosCategories = db.define(
   "todos_categories",
   {
     id: {
@@ -15,14 +17,22 @@ const Todos_categories = db.define(
       type: DataTypes.INTEGER,
       allowNull: false,
       field: "todo_id",
+      references: {
+        model: Todos,
+        key: "id",
+      },
     },
     categoryId: {
       type: DataTypes.INTEGER,
       allowNull: false,
       field: "category_id",
+      references: {
+        model: Categories,
+        key: "id",
+      },
     },
   },
   { timestamps: false }
 );
 
-module.exports = Todos_categories;
+module.exports = TodosCategories;
